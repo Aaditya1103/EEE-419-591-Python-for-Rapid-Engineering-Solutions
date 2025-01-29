6fe3f791064e432ab255b928a0778a6b1afdd859
Aaditya1103 <149060958+Aaditya1103@users.noreply.github.com>
2025-01-29 16:07:21 -0700

    Update hw1_12.py

--- Homeworks/HW-1/hw1_12.py
+++ Homeworks/HW-1/hw1_12.py
@@ -1,1 +1,33 @@
+################################################
+#     Name:- Aaditya Sakhardande               #
+#     ASU ID:- 1233720594                      #
+#     Email:- aadityasakhardande@gmail.com     #
+#     Date:- 01/29/2025                        #
+################################################
 
+import numpy as np
+
+def generate_prime_numbers(start, end):
+
+    primes = [2] 
+    # Loop through each number in the specified range to check if it's prime
+    for number in range(start, end + 1):
+        is_prime = True
+        
+        # Check divisibility
+        for p in primes:
+            if number % p == 0:  
+                is_prime = False
+                break
+            if p > np.sqrt(number): 
+                break
+        
+        # If the number is prime, append it to the list
+        if is_prime:
+            primes.append(number)
+    # Return the list of prime numbers
+    return primes
+
+# Generate prime numbers between 3 and 10000
+my_prime_list = generate_prime_numbers(3, 10000)
+print(my_prime_list)
